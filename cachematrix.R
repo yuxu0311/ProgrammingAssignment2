5329120efca3918a5982271b57e03c81dedbfa6a
Yuxu0311 <yuxu.lin@outlook.com>
2022-03-10 11:07:18 +0100

    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      i <- NULL
+      set <- function(matrix){
+            m <<- matrix
+            i <<- NULL
+      }
+      get <- function() {
+        m
+      }
+      setInverse <- function(inverse){
+        i <<- inverse
+      }
+      getInverse <- function() {
+        i
+      }
+      list(set = set, get = get, setInverse = setInverse, 
+           getInverse = getInverse)
 }
 
-
+cacheSolve <- function(x, ...) {
+  m <- x$getInverse()
+  
+  if(!is.null(m)) {
+    message("get chached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data) %*% data
+  x$setInverse(m)
+  m
+}
+  
+  )
+    
+  }
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
